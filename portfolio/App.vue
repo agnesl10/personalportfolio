d73cd7889d5ad113710e658cee8881ea1b6d9234
agnesl10 <158211772+agnesl10@users.noreply.github.com>
2026-05-12 11:11:12 -0400

    sdfsdf

---
+++ portfolio/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <Homepage />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Homepage from './components/homepage.vue'
+</script>
+
+<style scoped></style>
--- portfolio/components/homepage.vue
+++ portfolio/components/homepage.vue
@@ -1,5 +1,13 @@
 <template>
-  <div></div>
+  <div>
+    <div class="flex flex-col items-center justify-center h-screen">
+      <div class="flex items-baseline space-x-4 mb-6">
+        <h2 class="text-5xl serif-font">hi, i'm</h2>
+        <h1 class="text-7xl font-bold">agnes</h1>
+      </div>
+      <p class="text-xl text-center">i'm exploring web design and anything related to art.</p>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
