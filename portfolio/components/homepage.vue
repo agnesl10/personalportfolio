b2cf15a84dc5b70e132fe3d444140fa6278900e0
agnesl10 <158211772+agnesl10@users.noreply.github.com>
2026-05-19 11:02:52 -0400

    sdfsdf

--- portfolio/components/homepage.vue
+++ portfolio/components/homepage.vue
@@ -1,9 +1,23 @@
 <template>
-  <div class="relative h-screen overflow-hidden">
+  <div class="relative h-screen">
     <Navbar />
 
-    <div class="absolute right-[14%] top-1/2 -translate-y-1/2 rotate-15">
-      <img src="/digicam.png" alt="" class="w-150 md:w-187.5 lg:w-225 xl:w-250" />
+    <div class="absolute right-[10%] top-1/2 -translate-y-1/2">
+      <div class="relative w-[750px] max-w-none">
+        <div class="absolute z-0 overflow-hidden left-[10%] top-[10%] w-[63%] h-[80%] rotate-10">
+          <img src="/photo1.jpeg" class="w-full h-full object-cover" />
+        </div>
+
+        <img src="/digicam.png" class="relative z-10 w-full max-w-none block rotate-10" />
+      </div>
+    </div>
+
+    <div class="absolute inset-y-0 left-[10%] flex items-center">
+      <div class="flex flex-col items-start text-left">
+        <h2 class="text-6xl serif-font leading-none">hi, i'm</h2>
+
+        <h1 class="text-8xl title-font text-(--info) leading-none mt-2">agnes.</h1>
+      </div>
     </div>
   </div>
 </template>
@@ -11,5 +25,3 @@
 <script setup lang="ts">
 import Navbar from './navbar.vue'
 </script>
-
-<style scoped></style>
